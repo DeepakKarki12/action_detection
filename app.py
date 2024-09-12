1962e08b1919401790805eb7f0bc2fd5f1509610
DeepakKarki12 <indianarmypara9826@gmail.com>
2024-09-12 15:53:30 +0530

    port 5000 added

--- app.py
+++ app.py
@@ -37,5 +37,8 @@ if __name__ == '__main__':
 
     with app.app_context():
         db.create_all()
+    
     # app.run()
-    app.run(debug=True, port=8000)
+
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port)
