c9ed1a4852b9ce43a423c117da7ee198faa29ee2
DeepakKarki12 <indianarmypara9826@gmail.com>
2024-09-12 16:44:44 +0530

    removed if main condition from app.py

--- app.py
+++ app.py
@@ -40,14 +40,12 @@ def index():
     return render_template('index.html')
 
 print("1")
-if __name__ == '__main__':
-    print("2")
 
-    with app.app_context():
-        db.create_all()
-    
-    # app.run()
-    print("3")
-    port = int(os.environ.get('PORT', 5000)) 
-    print("4")
-    app.run(host='0.0.0.0', port=port)
+with app.app_context():
+    db.create_all()
+
+# app.run()
+print("3")
+port = int(os.environ.get('PORT', 5000)) 
+print("4")
+app.run(host='0.0.0.0', port=port)
