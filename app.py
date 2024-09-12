2ba29b87a367b8e27088cf27441f23ef67726b4c
DeepakKarki12 <indianarmypara9826@gmail.com>
2024-09-12 12:42:47 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,63 +1,17 @@
-# # app.py
-# from flask import Flask, render_template, request
-# from user import user_bp
-# from admin import admin_bp  # Import admin blueprint
-# from models import db
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
-# from pyngrok import ngrok
-# ngrok.set_auth_token('2j6yZx3KvVeSEOhGHG0bEjNdWB8_2ri6c36iDuZZxMJxxCD47')
-# # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# # postgresql://actiondetection_user:L0IBBidtXFP5ofA6wWxqqrOtNPIFa0yz@dpg-crh6li56l47c73c39fg0-a.oregon-postgres.render.com/actiondetection
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("database_url")
-# app.config['SECRET_KEY'] = os.getenv("secret_key")  # Replace with your secret key
-
-
-# # app = Flask(__name__)
-# # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("database_url")
-# # app.config['SECRET_KEY'] = os.environ.get("secret_key")  # Replace with your secret key
-
-# # Initialize the database
-# db.init_app(app)
-# def add_cache_control(response):
-#     if request.endpoint in ['admin.admin_dashboard', 'admin.logout']:
-#         response.headers['Cache-Control'] = 'no-store'
-#         response.headers['Pragma'] = 'no-cache'
-#         response.headers['Expires'] = '0'
-#     return response
-# # Register blueprints
-# app.register_blueprint(user_bp)
-# app.register_blueprint(admin_bp)  # Register admin blueprint
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     # Ensure the database tables are created
-
-#     with app.app_context():
-#         db.create_all()
-#     public_url = ngrok.connect(5000)
-#     print("Public URL:", public_url)
-#     app.run()
-#     #app.run(debug=True)
-
-
 # for hosting in render
 from flask import Flask, render_template, request
 from user import user_bp
 from admin import admin_bp  # Import admin blueprint
 from models import db
 import os
+# from dotenv import load_dotenv
+# load_dotenv()
 
 
 app = Flask(__name__)
 
 # Initialize the database
+
 def add_cache_control(response):
     if request.endpoint in ['admin.admin_dashboard', 'admin.logout']:
         response.headers['Cache-Control'] = 'no-store'
@@ -71,9 +25,12 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("database_url")
     app.config['SECRET_KEY'] = os.environ.get("secret_key")  # Replace with your secret key
+    
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("database_url")
+    # app.config['SECRET_KEY'] = os.getenv("secret_key")
+    
     db.init_app(app)
     app.register_blueprint(user_bp)
     app.register_blueprint(admin_bp)
